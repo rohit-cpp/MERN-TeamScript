562fe05c2e23082acd43100b5c4c2cc4d0bc1634
Rohit <rrgawanderohit@gmail.com>
2025-05-03 08:20:14 +0530

    added basic files and dependencies

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  return res.status(200).json({
+    message: "I am comming form backend",
+    success: true,
+  });
+});
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on PORT ${PORT}`);
+});
---
+++ frontend/src/pages/TeamSetting.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeamSetting = () => {
+  return <div>TeamSetting</div>;
+};
+
+export default TeamSetting;
